896f163e81d794ea8369fd40fb35e24570076644
giulia97-w <72927421+giulia97-w@users.noreply.github.com>
2023-03-22 00:10:06 +0100

    Add files via upload

--- src/main/metrics/MainClass.java
+++ src/main/metrics/MainClass.java
@@ -6,6 +6,7 @@ import org.eclipse.jgit.revwalk.RevCommit;
 
 import org.json.JSONException;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -48,14 +49,14 @@ public class MainClass {
         removeHalfRelease(releasesList, ticketList);
 
 
-        cleanTicketInconsistencies();
+        clearTicketListInconsistencies(ticketList);
         RetrieveGit.setBuilder(repo);
         logger.log(Level.INFO, "Numero ticket = {0}.", ticketList.size());
 
         Collections.reverse(ticketList); //reverse perchè è moving window
         Proportion.proportion(ticketList);
 
-        cleanTicketInconsistencies();   //devo rifarlo perchè, avendo settato nuovi IV, voglio togliere possibili incongruenze!
+        clearTicketListInconsistencies(ticketList);   //devo rifarlo perchè, avendo settato nuovi IV, voglio togliere possibili incongruenze!
 
         RetrieveGit.getJavaFiles(repoPath, releasesList);
 
@@ -189,7 +190,91 @@ public class MainClass {
         }
     }
 
+    public static void writeCSVBuggyness(List<Release> releasesList, String project) {
+
+        try (FileWriter fileWriter = new FileWriter(project.toLowerCase()+".Dataset.csv"))
+        {
+            //creo file csv.
+            fileWriter.append("RELEASE,FILENAME,SIZE,LOC_added,MAX_LOC_Added,AVG_LOC_Added,CHURN,MAX_Churn,AVG_Churn,NR,NAUTH,CHGSETSIZE,MAX_ChgSet,AVG_ChgSet,BUGGYNESS\n");
+            for (Release release : releasesList) {
+
+                for (JavaFile file : release.getFileList()) {
+                    //per ogni file appartenente alla release 'x'
+
+                	appendLocMetrics(fileWriter, file);
+                	appendChurnMetrics(fileWriter, file);
+                }
+            }
+        } catch (Exception ex) {
+            logger.log(Level.SEVERE, "Errore nella scrittura del file csc");
+        }
+
     }
+
+    private static void appendLocMetrics(FileWriter fileWriter, JavaFile file) throws IOException {
+        fileWriter.append(file.getName());
+        fileWriter.append(",");
+        fileWriter.append(file.getLOC().toString()); //LOC
+        fileWriter.append(",");
+        fileWriter.append(file.getLOCadded().toString()); //LOC_added
+        fileWriter.append(",");
+        if (file.getLOCadded().equals(0)) { //se non ho aggiunto nulla niente max e avg
+            fileWriter.append("0");
+            fileWriter.append(",");
+            fileWriter.append("0");
+        } else {
+            int maxLocAdded = Collections.max((file.getLocAddedList())); //prendo il max dalla lista
+            fileWriter.append(String.valueOf(maxLocAdded)); //scrivo tale massimo
+            fileWriter.append(",");
+            int avgLocAdded = (int)file.getLocAddedList().stream().mapToInt(Integer::intValue).average().orElse(0.0); //easy way to avg
+            fileWriter.append(String.valueOf(avgLocAdded));
+        }
+        fileWriter.append("\n");
+        fileWriter.flush();
+    }
+
+    private static void appendChurnMetrics(FileWriter fileWriter, JavaFile file) throws IOException {
+        fileWriter.append(file.getName());
+        fileWriter.append(",");
+        fileWriter.append(file.getChurn().toString());
+        fileWriter.append(",");
+        if (file.getChurn().equals(0)) {
+            fileWriter.append("0");
+            fileWriter.append(",");
+            fileWriter.append("0");
+        } else {
+            int maxChurn = Collections.max((file.getChurnList()));
+            fileWriter.append(String.valueOf(maxChurn));
+            fileWriter.append(",");
+            int avgChurn = (int) file.getChurnList().stream().mapToInt(Integer::intValue).average().orElse(0.0); //easy way
+            fileWriter.append(String.valueOf(avgChurn));
+        }
+        fileWriter.append(",");
+        fileWriter.append(file.getNr().toString());
+        fileWriter.append(",");
+        int size = file.getNAuth().size();
+        fileWriter.append(String.valueOf(size));
+        fileWriter.append(",");
+        fileWriter.append(file.getChgSetSize().toString());
+        fileWriter.append(",");
+        if (file.getChgSetSize().equals(0)) {
+            fileWriter.append("0");
+            fileWriter.append(",");
+            fileWriter.append("0");
+        } else {
+            int maxChgSet = Collections.max((file.getChgSetSizeList()));
+            fileWriter.append(String.valueOf(maxChgSet));
+            fileWriter.append(",");
+            int avgChgSet = (int) file.getChgSetSizeList().stream().mapToInt(Integer::intValue).average().orElse(0.0); //da calcolare
+            fileWriter.append(String.valueOf(avgChgSet));
+        }
+        fileWriter.append(",");
+        fileWriter.append(file.getBugg());
+        fileWriter.append("\n");
+        fileWriter.flush();
+    }
+
+
 
 
 }
