b7e0feefffd2f7c6b9cdc4a6047724cf34d15019
giulia97-w <72927421+giulia97-w@users.noreply.github.com>
2023-04-14 22:45:27 +0200

    Add files via upload

--- src/main/RetrieveGit.java
+++ src/main/RetrieveGit.java
@@ -159,7 +159,7 @@ public class RetrieveGit {
                 try {
                     return getDiffs(commit);
                 } catch (IOException e) {
-                    // TODO Auto-generated catch block
+                    
                     e.printStackTrace();
                 }
                 return null;
