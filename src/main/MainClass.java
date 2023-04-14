e48bb1f2d4ae2017330b9beefbab71ac10449d51
giulia97-w <72927421+giulia97-w@users.noreply.github.com>
2023-04-14 23:13:03 +0200

    Add files via upload

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,16 +1,21 @@
 
 package main;
 
+import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.DiffFormatter;
 import org.eclipse.jgit.diff.Edit;
 import org.eclipse.jgit.diff.RawTextComparator;
+import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectLoader;
 import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.treewalk.CanonicalTreeParser;
+import org.eclipse.jgit.treewalk.EmptyTreeIterator;
 import org.eclipse.jgit.treewalk.TreeWalk;
 import org.eclipse.jgit.util.io.DisabledOutputStream;
 import org.json.JSONException;
@@ -21,6 +26,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -422,8 +428,8 @@ public class MainClass {
     	
         releasesListBookkeeper = RetrieveJira.getListRelease(PROJECT);
         releasesListOpenjpa = RetrieveJira.getListRelease(PROJECT1);
-        commitListBookkeeper = RetrieveGit.getAllCommit(releasesListBookkeeper, Paths.get(percorso + PROJECT.toLowerCase()));
-        commitListOpenjpa = RetrieveGit.getAllCommit(releasesListOpenjpa, Paths.get(percorso + PROJECT1.toLowerCase()));
+        commitListBookkeeper = getAllCommits(releasesListBookkeeper, Paths.get(percorso + PROJECT.toLowerCase()));
+        commitListOpenjpa = getAllCommits(releasesListOpenjpa, Paths.get(percorso + PROJECT1.toLowerCase()));
         ticketListBookkeeper = RetrieveJira.getTickets(releasesListBookkeeper, PROJECT);
         ticketListOpenjpa = RetrieveJira.getTickets(releasesListOpenjpa, PROJECT1);
         
@@ -432,13 +438,13 @@ public class MainClass {
         linkFunction();
         removeReleaseBeforeHalf(releasesListBookkeeper, ticketListBookkeeper);
         checkTicketBookkeeper(); 
-        RetrieveGit.setBuilder(percorso + PROJECT.toLowerCase() + endPath);
+        setBuilder(percorso + PROJECT.toLowerCase() + endPath);
         logger.log(Level.INFO, "Numero ticket = {0}.", ticketListBookkeeper.size());
         Collections.reverse(ticketListBookkeeper); 
         Proportion.proportion(ticketListBookkeeper);
         checkTicketBookkeeper();  
-        RetrieveGit.getJavaFiles(Paths.get(percorso + PROJECT.toLowerCase()), releasesListBookkeeper);
-        RetrieveGit.checkBuggyness(releasesListBookkeeper, ticketListBookkeeper); 
+        getJavaFiles(Paths.get(percorso + PROJECT.toLowerCase()), releasesListBookkeeper);
+        checkBuggyness(releasesListBookkeeper, ticketListBookkeeper); 
         getRepo(releasesListBookkeeper, percorso + PROJECT.toLowerCase() + endPath);
         createCSV(releasesListBookkeeper, PROJECT.toLowerCase());
 
@@ -447,13 +453,13 @@ public class MainClass {
         linkFunction();
         removeReleaseBeforeHalf(releasesListOpenjpa, ticketListOpenjpa);
         checkTicketOpenjpa(); 
-        RetrieveGit.setBuilder(percorso + PROJECT1.toLowerCase() + endPath);
+        setBuilder(percorso + PROJECT1.toLowerCase() + endPath);
         logger.log(Level.INFO, "Numero ticket = {0}.", ticketListOpenjpa.size());
         Collections.reverse(ticketListOpenjpa);
         Proportion.proportion(ticketListOpenjpa);
         checkTicketOpenjpa(); 
-        RetrieveGit.getJavaFiles(Paths.get(percorso + PROJECT1.toLowerCase()), releasesListOpenjpa);
-        RetrieveGit.checkBuggyness(releasesListOpenjpa, ticketListOpenjpa); 
+        getJavaFiles(Paths.get(percorso + PROJECT1.toLowerCase()), releasesListOpenjpa);
+        checkBuggyness(releasesListOpenjpa, ticketListOpenjpa); 
         getRepo(releasesListOpenjpa, percorso + PROJECT1.toLowerCase() + endPath);
         createCSV(releasesListOpenjpa, PROJECT1.toLowerCase());
 
@@ -481,7 +487,7 @@ public class MainClass {
                     diff.setDiffComparator(RawTextComparator.DEFAULT);
                     diff.setDetectRenames(true);
                     String authName = commit.getAuthorIdent().getName();
-                    List<DiffEntry> diffs = RetrieveGit.getDiffs(commit);
+                    List<DiffEntry> diffs = getDiffs(commit);
                     if (diffs != null) {
                         analyzeDiffEntryMetrics(diffs, filesJavaListPerRelease, authName, diff);
                     }
@@ -566,7 +572,7 @@ public class MainClass {
         }
         file.setLOCadded(file.getLOCadded() + locAdded);
         addToLocAddedList(file, locAdded);
-        file.setChgSetSize(file.getChgSetSize() + numTouchedClass);
+        
         file.setChurn(file.getChurn() + churn);
         addToChurnList(file, churn);
     }
@@ -592,12 +598,7 @@ public class MainClass {
         listAuth.add(authName);
         javaFile.setNAuth(listAuth);
     }
-    private static void setChgSetSize(JavaFile javaFile, int numTouchedClass) {
-        javaFile.setChgSetSize(numTouchedClass);
-        List<Integer> chgSetSizeList = new ArrayList<>();
-        chgSetSizeList.add(numTouchedClass);
-        javaFile.setChgSetSizeList(chgSetSizeList);
-    }
+    
     private static void setLocAdded(JavaFile javaFile, int locAdded) {
         javaFile.setLOCadded(locAdded);
         List<Integer> locAddedList = new ArrayList<>();
@@ -616,7 +617,7 @@ public class MainClass {
     public static void applyMetricsNewFile(JavaFile javaFile, int numTouchedClass, int locAdded, int churn, List<JavaFile> fileList, String authName) {
         setNr(javaFile);
         setNAuth(javaFile, authName);
-        setChgSetSize(javaFile, numTouchedClass);
+        
         setLocAdded(javaFile, locAdded);
         setChurn(javaFile, churn);
         addToFileList(fileList, javaFile);
@@ -639,7 +640,7 @@ public class MainClass {
         updateLOCadded(javaFile, fileInRelease);
         updateChurn(javaFile, fileInRelease);
         updateNAuth(javaFile, fileInRelease);
-        updateChgSetSize(javaFile, fileInRelease);
+        
     }
 
     private static void updateLOCadded(JavaFile javaFile, JavaFile fileInRelease) {
@@ -668,13 +669,7 @@ public class MainClass {
         fileInRelease.setNAuth(nAuthList);
     }
 
-    private static void updateChgSetSize(JavaFile javaFile, JavaFile fileInRelease) {
-        int chgSetSize = javaFile.getChgSetSize();
-        fileInRelease.setChgSetSize(fileInRelease.getChgSetSize() + chgSetSize);
-        List<Integer> chgSetSizeList = fileInRelease.getChgSetSizeList();
-        chgSetSizeList.addAll(javaFile.getChgSetSizeList());
-        fileInRelease.setChgSetSizeList(chgSetSizeList);
-    }
+    
 
     
 
@@ -685,7 +680,227 @@ public class MainClass {
             try (InputStream input = loader.openStream()) {
                 BufferedReader readerNew = new BufferedReader(new InputStreamReader(input));
                 return (int) readerNew.lines().count();
+            }}
+        }
+        
+        private static Repository repository;
+        
+
+
+        
+
+        public static List<RevCommit> getAllCommits(List<Release> releases, Path repositoryPath) throws GitAPIException, IOException {
+            List<RevCommit> commits = new ArrayList<>();
+            try (Git git = Git.open(repositoryPath.toFile())) {
+                Iterable<RevCommit> logEntries = git.log().all().call();
+                for (RevCommit commit : logEntries) {
+                    commits.add(commit);
+                    updateReleaseWithCommit(commit, releases);
+                }
+            }
+            return commits;
+        }
+
+        private static void updateReleaseWithCommit(RevCommit commit, List<Release> releases) {
+            Release release = getReleaseForCommit(commit, releases);
+            if (release != null) {
+                release.addCommit(commit);
             }
         }
+
+        
+
+
+        private static Release getReleaseForCommit(RevCommit commit, List<Release> releaseList) {
+            LocalDateTime commitDate = commit.getAuthorIdent().getWhen().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            int releaseIndex = RetrieveJira.compareDateVersion(commitDate, releaseList);
+            return releaseList.stream()
+                              .filter(release -> release.getIndex() == releaseIndex)
+                              .findFirst()
+                              .orElse(null);
+        }
+
+
+
+        private static Git initGitRepository(Path repoPath) throws IOException, IllegalStateException, GitAPIException {
+            return Git.init().setDirectory(repoPath.toFile()).call();
+        }
+        private static void getJavaFilesForRelease(Git git, Release release) throws IOException {
+            List<String> javaFileNames = new ArrayList<>();
+            for (RevCommit commit : release.getCommitList()) {
+                ObjectId treeId = commit.getTree();
+                try (TreeWalk treeWalk = new TreeWalk(git.getRepository())) {
+                    treeWalk.reset(treeId);
+                    treeWalk.setRecursive(true);
+
+                    while (treeWalk.next()) {
+                        addJavaFile(treeWalk, release, javaFileNames);
+                    }
+                }
+            }
+        }
+        public static void getJavaFiles(Path repoPath, List<Release> releasesList) throws IOException, IllegalStateException, GitAPIException {
+            Git git = initGitRepository(repoPath);
+
+            for (Release release : releasesList) {
+                getJavaFilesForRelease(git, release);
+                setFileListIfNeeded(release, releasesList);
+            }
+        }
+
+
+        private static void setFileListIfNeeded(Release release, List<Release> releasesList) {
+            if (release.getFile().isEmpty() && !releasesList.isEmpty()) {
+                Release previousRelease = releasesList.get(releasesList.size() - 1);
+                release.setFileList(previousRelease.getFile());
+            }
+        }
+
+       
+
+
+
+        private static void addJavaFile(TreeWalk treeWalk, Release release, List<String> fileNameList) throws IOException {
+            // Ottieni il nome del file
+            String filename = treeWalk.getPathString();
+            // Se il file ha estensione .java e non è già stato aggiunto alla release
+            if (filename.endsWith(".java") && !fileNameList.contains(filename)) {
+                    // Crea una nuova istanza di JavaFile con il nome del file
+                    JavaFile file = new JavaFile(filename);
+                    // Imposta gli attributi di default per il nuovo file
+                    setDefaultJavaFileAttributes(treeWalk, file);
+                    // Aggiungi il file alla release
+                    release.getFile().add(file);
+                    // Aggiungi il nome del file alla lista dei file
+                    fileNameList.add(filename);
+            }
+    }
+
+
+        private static void setDefaultJavaFileAttributes(TreeWalk treeWalk, JavaFile file) throws IOException {
+            // Imposta gli attributi di default per un nuovo file Java
+            file.setBugg("false");
+            file.setNr(0);
+            file.setNAuth(new ArrayList<>());
+            file.setLOCadded(0);
+            file.setLocAddedList(new ArrayList<>());
+            file.setChurn(0);
+            file.setChurnList(new ArrayList<>());
+            // Imposta la dimensione del file in linee di codice
+            int loc = MainClass.size(treeWalk, repository);
+            file.setLOC(loc);
+        }
+
+
+
+        public static void checkBuggyness(List<Release> releaseList, List<Ticket> ticketList) throws IOException {
+            for (Ticket ticket : ticketList) {
+                verify(ticket, releaseList);
+            }
+        }
+
+        private static void verify(Ticket ticket, List<Release> releaseList) throws IOException {
+            List<Integer> av = ticket.getAV();
+            ticket.getCommitList().stream()
+                .map(commit -> {
+                    try {
+                        return getDiffs(commit);
+                    } catch (IOException e) {
+                        
+                        e.printStackTrace();
+                    }
+                    return null;
+                })
+                .filter(Objects::nonNull)
+
+                .forEach(diffs -> diff(diffs, releaseList, av));
+        }
+
+
+
+        private static DiffFormatter initializeDiffFormatter() {
+            DiffFormatter diff = new DiffFormatter(DisabledOutputStream.INSTANCE);
+            diff.setRepository(repository);
+            diff.setContext(0);
+            diff.setDetectRenames(true);
+            return diff;
+        }
+        private static List<DiffEntry> getDiffsBetweenCommits(RevCommit parent, RevCommit commit, DiffFormatter diff) throws IOException {
+            return diff.scan(parent.getTree(), commit.getTree());
+        }
+        private static List<DiffEntry> getDiffsWithEmptyTree(RevCommit commit, DiffFormatter diff) throws IOException {
+            try (RevWalk rw = new RevWalk(repository)) {
+    			return diff.scan(new EmptyTreeIterator(), new CanonicalTreeParser(null, rw.getObjectReader(), commit.getTree()));
+    		}
+        }
+        public static List<DiffEntry> getDiffs(RevCommit commit) throws IOException {
+            DiffFormatter diff = initializeDiffFormatter();
+            List<DiffEntry> diffs;
+            if (commit.getParentCount() != 0) {
+                RevCommit parent = (RevCommit) commit.getParent(0).getId();
+                diffs = getDiffsBetweenCommits(parent, commit, diff);
+            } else {
+                diffs = getDiffsWithEmptyTree(commit, diff);
+            }
+            return diffs;
+        }
+
+
+
+        public static void diff(List<DiffEntry> diffs, List<Release> releasesList, List<Integer> av) {
+            for (DiffEntry diff : diffs) {
+                if (isBuggyDiffEntry(diff)) {
+                    String file = getFilePathFromDiffEntry(diff);
+                    setBuggyness(file, releasesList, av);
+                }
+            }
+        }
+
+        private static boolean isBuggyDiffEntry(DiffEntry diff) {
+            String type = diff.getChangeType().toString();
+            return diff.toString().contains(".java") && (type.equals("MODIFY") || type.equals("DELETE"));
+        }
+
+        private static String getFilePathFromDiffEntry(DiffEntry diff) {
+            if (diff.getChangeType() == DiffEntry.ChangeType.DELETE || diff.getChangeType() == DiffEntry.ChangeType.RENAME) {
+                return diff.getOldPath();
+            } else {
+                return diff.getNewPath();
+            }
+        }
+
+
+        public static void setBuggyness(String file, List<Release> releasesList, List<Integer> av) {
+            for (Release release : releasesList) {
+                setBuggynessForRelease(file, release, av);
+            }
+        }
+
+        private static void setBuggynessForRelease(String file, Release release, List<Integer> av) {
+            if (av.contains(release.getIndex())) {
+                setBuggynessForJavaFile(file, release.getFile());
+            }
+        }
+
+        private static void setBuggynessForJavaFile(String file, List<JavaFile> javaFiles) {
+            for (JavaFile javaFile : javaFiles) {
+                if (javaFile.getName().equals(file)) {
+                    javaFile.setBugg("true");
+                }
+            }
+        }
+
+
+        /*Ticket mi dice che c'è un bug, questo bug ha toccato le release x,y,z. Ricordo che prendo tutti ticket risolti, ovvero so la release in qui li ho fixati.
+        Il ticket include dei commit, i quali vanno a modificare dei file.java. Li modifico perchè quei file=classi hanno dei problemi, e li hanno dalla release x.
+        Allora il file.java nelle release x,y,z avevano problemi, ovvero erano buggy.*/
+
+        //utility di setup
+        public static void setBuilder(String repo) throws IOException {
+            FileRepositoryBuilder repositoryBuilder = new FileRepositoryBuilder();
+            repository = repositoryBuilder.setGitDir(new File(repo)).readEnvironment() // scan environment GIT_* variables
+                    .findGitDir() // scan up the file system tree
+                    .setMustExist(true).build();
+        
     }
 }
